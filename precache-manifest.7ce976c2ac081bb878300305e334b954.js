38bf7a2f3fd38db087a82c1f0edf2e28a1929f80
jamiejamiebobamie <jmccrory@vt.edu>
2020-02-19 21:52:48 -0800

    Updates

---
+++ precache-manifest.7ce976c2ac081bb878300305e334b954.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c1ec936a84e3d12d7168f1ff4209e996",
+    "url": "/easy-shop/index.html"
+  },
+  {
+    "revision": "c77a9fc1e74fc0578f39",
+    "url": "/easy-shop/static/css/main.4b065abb.chunk.css"
+  },
+  {
+    "revision": "36242458337d3c851f2d",
+    "url": "/easy-shop/static/js/2.ebea9fbf.chunk.js"
+  },
+  {
+    "revision": "98061d712500f07672e3581cfdcd5a24",
+    "url": "/easy-shop/static/js/2.ebea9fbf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c77a9fc1e74fc0578f39",
+    "url": "/easy-shop/static/js/main.6aa725f0.chunk.js"
+  },
+  {
+    "revision": "1602a026eb09fedd758f",
+    "url": "/easy-shop/static/js/runtime-main.5da49bf5.js"
+  }
+]);
