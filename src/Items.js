2779d426fadc102e15c1dd4cc918dd8a37f65f98
jamiejamiebobamie <jmccrory@vt.edu>
2020-02-19 22:58:00 -0800

    edited the image

--- src/Items.js
+++ src/Items.js
@@ -12,7 +12,7 @@ function Items(props){
             <div className="item-text-category">{props.category}</div>
         </li>
         <img className="itemImg"
-             src="https://cdn0.iconfinder.com/data/icons/black-logistics-icons/256/Add_product_arrow.png"
+             src="./imgs/product.png"
              alt=""/>
         </button>
     )
