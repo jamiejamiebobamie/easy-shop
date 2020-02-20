864504a648c4a4b9d4215aaf7b876e8354a7316b
jamiejamiebobamie <jmccrory@vt.edu>
2020-02-19 22:09:58 -0800

    push

--- src/Items.js
+++ src/Items.js
@@ -11,7 +11,7 @@ function Items(props){
             <div className="item-text-price">${props.price}</div>
             <div className="item-text-category">{props.category}</div>
         </li>
-        <img className="itemImg" src="/imgs/product.png" alt=""/>
+        <img className="itemImg" src="https://images.freshop.com/00022400623740/26f7d9faef72f66b1f534111b9476db5_medium.png" alt=""/>
         </button>
     )
 }
